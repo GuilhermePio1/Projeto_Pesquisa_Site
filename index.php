1ede6f244b53f591875c1cef604bd14efd1f6378
Lucas <bixopiruleta62@gmail.com>
2021-02-16 10:57:24 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <script src="https://kit.fontawesome.com/290f3f5eab.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="center">
+            <div class="logo left">
+                <a href="">marca</a>
+            </div><!--logo-->
+            <nav class="desktop right">
+                <ul>
+                    <li><a href="">sobre</a></li>
+                    <li><a href="">contato</a></li>
+                    <li><a href="">download</a></li>
+                </ul>
+            </nav><!--desktop-->
+            <nav class="mobile">
+                <div class="botao-menu-mobile">
+                    <i class="fas fa-bars"></i>
+                </div><!--botao-menu-mobile-->
+                <ul>
+                    <li><a href="">sobre</a></li>
+                    <li><a href="">contato</a></li>
+                    <li><a href="">download</a></li>
+                </ul>
+            </nav><!--mobile-->
+
+        </div><!--center-->
+    </header>
+</body>
+</html>
